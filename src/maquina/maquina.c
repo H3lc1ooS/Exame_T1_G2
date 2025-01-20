258478664c29f0968881ceefc3c4ab7319e5bd5b
Hélcio Paulino Marcelino da Silva <sandbox@pop-os.localdomain>
2025-01-20 19:53:11 +0100

    comecei a adicionar o menu

--- src/maquina/maquina.c
+++ src/maquina/maquina.c
@@ -3,12 +3,12 @@
 
 // Definição do struct para a máquina de produção
 struct maquina{
-    char *nome;              // Nome da máquina
-    char *tipo;              // Tipo da máquina (enchimento, embalamento, empilhamento)
-    float tempo_processamento;  // Tempo de processamento por unidade (em segundos, por exemplo)
+    char *nome;
+    char *tipo; // (enchimento, embalamento, empilhamento)
+    float tempo_processamento;  // Tempo de processamento por unidade (em segundos)
     int unidades_processadas;   // Quantidade de unidades processadas em um dia
     Fila *fila_pacotes;
-    int tamanho_fila;           // Tamanho atual da fila
-    int estado;                 // Estado atual (ex.: "1 -> ativo", "2 -> parado", "3 -> manutenção")
-    int producao_diaria_maxima; // Capacidade máxima de produção por dia
+    int tamanho_fila;
+    int estado; // Estado atual (ex.: "1 -> activa", "2 -> parada", "3 -> manutenção")
+    int producao_diaria_maxima;
 };
--- src/pacote/pacote.c
+++ src/pacote/pacote.c
@@ -8,7 +8,7 @@ struct pacote
     float volumeEmLitros;
     float pesoEmGramas;
     int estadoPacote; // Indica o estado do pacote (0 [vazio] ou 1 [cheio])
-    int condicoes; // Indica se o pacote está dentro dos padrões (0 [ko] ou 1 [ok])
+    int condicoes;    // Indica se o pacote está dentro dos padrões (0 [ko] ou 1 [ok])
 };
 
 struct no
@@ -27,3 +27,32 @@ struct pilha
 {
     NoPacote *topo;
 };
+
+void menu(int opcao)
+{
+    printf("|------------------------------------------------------------|\n");
+    printf("|---------------------Escolha uma opção:---------------------|\n");
+    printf("Escolha uma opção:\n");
+}
+
+int main()
+{
+    menu(0);
+    /*
+    FILE *lista_pacotes = fopen("../../files/lista_pacotes.txt", "r");
+    char buffer[255];
+
+    if (lista_pacotes == NULL)
+        printf("Erro: Não é possível abrir o ficheiro!\n");
+    else
+    {
+        while (fgets(buffer, 255, lista_pacotes) != NULL)
+        {
+            printf("%s", buffer);
+        }
+        printf("\n");
+    }
+
+    fclose(lista_pacotes);*/
+    return (0);
+}
